b57744e728b277ffaeb8e404848efc9c451b194d
Dave Rothfarb <dave.rothfarb.webdev@gmail.com>
2022-06-15 00:34:41 -0400

    Working my way through Specimen component: added type, dateTime, patient, setPatient props with 2 tests so far

--- src/components/Specimen.js
+++ src/components/Specimen.js
@@ -1,10 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
 import Button from 'react-bootstrap/Button';
 
-const Specimen = () => {
+const Specimen = ({ 
+  type, 
+  collectedDateTime, 
+  patient, 
+  setPatient 
+}) => {
+  const [variant, setVariant] = useState("info");
+
+  const handleClick = (e) => {
+    setVariant('warning');
+  }
+
   return (
-    <Button>Test</Button>
+    <Button variant={ variant } onClick={ handleClick }>
+      <p>{ type }</p>
+      <p>{ collectedDateTime }</p>
+    </Button>
   );
+}
+
+Specimen.propTypes = {
+  type: PropTypes.string.isRequired,
+  collectedDateTime: PropTypes.string.isRequired,
+  patient: PropTypes.string.isRequired,
+  setPatient: PropTypes.func.isRequired
 }
 
 export default Specimen;
--- src/components/__tests__/Specimen.test.js
+++ src/components/__tests__/Specimen.test.js
@@ -1,7 +1,39 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import Specimen from '../Specimen';
 
-test('Should render a button', () => {
-  expect(render(<Specimen />).getByRole('button')).toBeInTheDocument();
+const collectedDateTime = "2015-02-25T00:00:00-06:00";
+
+test('Should render a button with "type" and "dateTime" text displayed', () => {
+
+  const specimen = render(
+    <Specimen
+      type="searchset"
+      patient="Patient/1194"
+      collectedDateTime={ collectedDateTime }
+      setPatient={ jest.fn() }
+    />
+  );
+  expect(specimen.getByRole('button')).toBeInTheDocument();
+  expect(specimen.getByText('searchset')).toBeInTheDocument();
+  expect(specimen.getByText(collectedDateTime)).toBeInTheDocument();
 });
+
+test('Should change button class from "btn-info" to "btn-warning" when clicked', () => {
+  const specimen = render(
+    <Specimen
+      type="searchset"
+      patient="Patient/1194"
+      collectedDateTime={ collectedDateTime }
+      setPatient={ jest.fn() }
+    />
+  );
+  const specButton = screen.getByRole('button');
+  expect(specButton).toHaveClass('btn-info');
+  userEvent.click(specButton);
+  expect(specButton).toHaveClass('btn-warning');
+});
+
+
+
