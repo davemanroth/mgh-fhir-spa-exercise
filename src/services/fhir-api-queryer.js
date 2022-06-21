f155791592d53d32079d540d5becc847837f4cdb
Dave Rothfarb <dave.rothfarb.webdev@gmail.com>
2022-06-20 23:08:20 -0400

    Restructured FHIR API queryer to be module with multiple query types: one for bundle, one for immunizations per patient ID

--- src/services/fhir-api-queryer.js
+++ src/services/fhir-api-queryer.js
@@ -1,17 +1,32 @@
-const fhirApiQueryer = async (args) => {
+const fhirApiQueryer = () => {
   const init = {
     method: "GET",
     headers: { "Content-Type": "application/fhir+json" }
   };
 
-  try {
-    const res = await fetch(`http://hapi.fhir.org/baseR4/${args}`, init);
-    return res.json();
+  const getBundle = async (type) => {
+    try {
+      const bundle = await fetch(`http://hapi.fhir.org/baseR4/${type}`, init);
+      return bundle.json();
+    }
+    catch(e) {
+      console.error(e);
+    }
   }
-  catch(e) {
-    console.error(e);
+
+  const getImmunizations = async (patientID) => {
+    try {
+      const immunizations = await fetch(`http://hapi.fhir.org/baseR4/Immunization?patient=${patientID}`, init);
+      return immunizations.json();
+    }
+    catch(e) {
+      console.error(e);
+    }
   }
+  return { getBundle, getImmunizations };
 }
+
+  
 /*
 */
 
--- src/services/fhir-api.test.js
+++ src/services/fhir-api.test.js
@@ -2,6 +2,8 @@ import fhirApiQueryer from './fhir-api-queryer';
 import mockImmunizationBundle from '../__mocks__/immunization_bundle';
 import mockPatientResource from '../__mocks__/patient_resource';
 
+const queryer = fhirApiQueryer();
+
 describe('FHIR API tests', () => {
   beforeEach( () => {
     fetch.resetMocks();
@@ -10,38 +12,60 @@ describe('FHIR API tests', () => {
   it('should return a Bundle resource type with "Immunization" or "Patient" requests', () => {
     fetch.mockResponse(JSON.stringify(mockImmunizationBundle));
     ["Immunization", "Patient"].forEach( (type) => {
-      fhirApiQueryer(type).then( (res) => {
+      queryer.getBundle(type).then( (res) => {
         expect(res.hasOwnProperty("resourceType")).toBe(true);
         expect(res.resourceType).toEqual("Bundle");
       });
     });//forEach
   });
 
-  it('should return a Patient resource type', () => {
-    fetch.mockResponse(JSON.stringify(mockPatientResource));
-    fhirApiQueryer("Patient/1471a46f-ae47-4128-bc0c-81adf0ea417b").then( (res) => {
-      expect(res.hasOwnProperty("resourceType")).toBe(true);
-      expect(res.resourceType).toEqual("Patient");
-    });
-  });
 
   it('should have a non-zero length "entry" array property for Immunization bundle', () => {
     fetch.mockResponse(JSON.stringify(mockImmunizationBundle));
-    fhirApiQueryer("Immunization").then( (res) => {
+    queryer.getBundle("Immunization").then( (res) => {
       expect(res.hasOwnProperty("entry")).toBe(true);
       expect(res.entry.length).toBeGreaterThan(0);
     });
   });
 
+  it('should have name of vaccine for Immunization bundle', () => {
+    fetch.mockResponse(JSON.stringify(mockImmunizationBundle));
+    queryer.getBundle("Immunization").then( (res) => {
+      const resource = res.entry[0].resource;
+      expect(resource.hasOwnProperty("vaccineCode")).toBe(true);
+      expect(resource.vaccineCode.hasOwnProperty("text")).toBe(true);
+      expect(resource.vaccineCode.text).toBe("Influenza, seasonal, injectable, preservative free");
+    });
+  });
+
   it('should have an occurrenceDateTime prop for Immunization bundle', () => {
     fetch.mockResponse(JSON.stringify(mockImmunizationBundle));
-    fhirApiQueryer("Immunization").then( (res) => {
+    queryer.getBundle("Immunization").then( (res) => {
       const resource = res.entry[0].resource;
       expect(resource.hasOwnProperty("occurrenceDateTime")).toBe(true);
       expect(resource.occurrenceDateTime).toBe("2013-12-27T12:20:53.090-06:00");
     });
   });
 
+  it('should find 3 immunizations for a specific patient', async () => {
+    const id = "1471a46f-ae47-4128-bc0c-81adf0ea417b";
+    const patientImm = mockImmunizationBundle.entry.slice(0, 3);
+    fetch.mockResponse(JSON.stringify(patientImm));
+    const result = await queryer.getImmunizations(id)
+      .catch( (e) => {
+        console.error(e);
+      });
+    const hasSameId = result.every( (immunization) => {
+      const reference = immunization.resource.patient.reference;
+      return reference.split("/").pop() === id;
+    });
+    expect(hasSameId).toBeTruthy();
+    expect(result.length).toBe(3);
+  });
+
+
+
+/*
   it('should have non-empty "gender" property for Patient resource type', () => {
     fetch.mockResponse(JSON.stringify(mockPatientResource));
     fhirApiQueryer("Patient/1471a46f-ae47-4128-bc0c-81adf0ea417b").then( (res) => {
@@ -57,5 +81,6 @@ describe('FHIR API tests', () => {
       expect(res.birthDate).toEqual("1952-06-12");
     });
   });
+  */
 });
 
